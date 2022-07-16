ce6a38e3278bb51ac62476647ac024ced1325bdc
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-16 16:27:09 -0600

    Add the click listener

---
+++ js/app.js
@@ -0,0 +1,101 @@
+let menu_items = [
+    `https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80`,
+    `https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=975&q=80`,
+    `https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=974&q=80`,
+    `https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80`,
+    `https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80`,
+    `https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`,
+    `https://images.unsplash.com/photo-1433086966358-54859d0ed716?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80`,
+    `https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`,
+    `https://images.unsplash.com/photo-1586348943529-beaae6c28db9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80`,
+    `https://images.unsplash.com/photo-1518173946687-a4c8892bbd9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80`,
+]
+
+function changeImg(details) {
+    let imageReceived = ``;
+    for(let counter = 0; counter < menu_items.length; counter++){
+        imageReceived = menu_items[counter];
+        let section = document.getElementById(`main-first-section`);
+        section.insertAdjacentHTML(`beforeEnd`, `<img src="${imageReceived}">`);
+    }
+    let buttonTag = document.querySelector(`button`);
+    buttonTag.remove();
+}
+
+let buttonTag = document.querySelector(`button`);
+buttonTag.addEventListener(`click`, changeImg);
+
+/*-------------------------------------------- */
+
+let menu_items_objects = [
+    {
+        title: `Our product`,
+        image_source: `https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80`,
+        price: 60.80
+    },
+    {
+        title: `Our product`,
+        image_source: `https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=975&q=80`,
+        price: 60.80
+    },
+    {
+        title: `Our product`,
+        image_source: `https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=974&q=80`,
+        price: 60.80
+    },
+    {
+        title: `Our product`,
+        image_source: `https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80`,
+        price: 60.80
+    },
+    {
+        title: `Our product`,
+        image_source: `https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80`,
+        price: 60.80
+    },
+    {
+        title: `Our product`,
+        image_source: `https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`,
+        price: 60.80
+    },
+    {
+        title: `Our product`,
+        image_source: `https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`,
+        price: 60.80
+    },
+    {
+        title: `Our product`,
+        image_source: `https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`,
+        price: 60.80
+    },
+    {
+        title: `Our product`,
+        image_source: `https://images.unsplash.com/photo-1586348943529-beaae6c28db9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80`,
+        price: 60.80
+    },
+    {
+        title: `Our product`,
+        image_source: `https://images.unsplash.com/photo-1518173946687-a4c8892bbd9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80`,
+        price: 60.80
+    },
+];
+
+function changeImgObject(details){
+    let imageReceived = ``;
+    for(let counter = 0; counter < menu_items_objects.length; counter++){
+        imageReceived = menu_items_objects[counter][`image_source`];
+        let section = document.getElementById(`main-first-other-section`);
+        section.insertAdjacentHTML(`beforeEnd`, 
+        `<div> 
+            <img src="${imageReceived}">
+            <h2>This is the image</h2>
+            <p>$ 8.99</p>
+        </div>`
+        );
+    }
+    let buttonObject = document.getElementById(`main-other-button`)
+    buttonObject.remove();
+}
+
+let buttonObject = document.getElementById(`main-other-button`)
+buttonObject.addEventListener(`click`, changeImgObject);
